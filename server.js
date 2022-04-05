dade1a1d22f04a30b4dd30bcad3e7e451dd553b2
ivantanghw <ivantanghw@gmail.com>
2022-04-05 14:50:26 +0800

    final update on 20220405

--- server.js
+++ server.js
@@ -3,6 +3,17 @@ const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
 
+// Import controllers
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+
+// GRPC
+const {ClarifaiStub, grpc} = require("clarifai-nodejs-grpc");
+const stub = ClarifaiStub.grpc();
+const metadata = new grpc.Metadata();
+metadata.set("authorization", "Key 3ef7c5cbf81b4bcc863b3abef3fc0834");
+
 const db = knex({
     client: 'pg',
     connection: {
@@ -14,44 +25,7 @@ const db = knex({
     }
   });
 
-/* db is working
-db.select().from('users').then(data => {
-    console.log(data) --> returns all data inside 'users'
-});
-*/
-
-
 const app = express();
-
-/* We are not needing this database
-const database = {
-    users: [
-        {
-            id: '123',
-            name: 'John',
-            email:'john@gmail.com',
-            password: 'cookies',
-            entries: 0,
-            joined: new Date()
-        },
-        {
-            id: '124',
-            name: 'Sally',
-            email:'sally@gmail.com',
-            password: 'bananas',
-            entries: 0,
-            joined: new Date()
-        }
-    ],
-    login: [
-        {
-            id: '987',
-            hash: '',
-            email: 'john@gmail.com'
-        }
-    ]
-}
-*/
 
 app.use(express.json());
 app.use(cors());
@@ -62,110 +36,10 @@ app.get('/', (req, res) => {
     .catch(err => res.status(400).json('Unable to access database'))
 })
 
-// Sign in route
-app.post('/signin', (req, res) => {
-    db.select('email', 'hash').from('login')
-        .where('email','=', req.body.email)
-        .then(data => {
-            const isPwValid = bcrypt.compareSync(req.body.password, data[0].hash);
-            if (isPwValid) {
-                return db.select('*').from('users')
-                    .where('email', '=', req.body.email)
-                    .then(user => {
-                        res.json(user[0])
-                    })
-                    .catch(err => res.status(400).json('Unable to get user information'))
-            } else {
-                res.status(400).json('User not found')
-            }
-        })
-        .catch(err => res.status(400).json('Wrong credentials'))
-    /* Old Method
-    if (req.body.email === database.users[0].email && req.body.password === database.users[0].password) {
-        res.json(database.users[0]);
-    } else {
-        res.status(400).json('Error loggin in')
-    }
-    */
-})
-
-//    bcrypt.hash(password, null, null, function(err, hash) {
-//    console.log(`A new user is joined, password is: ${hash}`);
-// });
-//Bcrypt to encrypt passwords
-
-// Load hash from your password DB.
-// bcrypt.compare("bacon", hash, function(err, res) {
-//     // res == true
-// });
-// bcrypt.compare("veggies", hash, function(err, res) {
-//     // res = false
-// });
-
-// Register route
-app.post('/register', (req, res) => {
-    const { email, name, password } = req.body;
-    const hash = bcrypt.hashSync(password);
-    db.transaction(trx => {
-        // with trx - syntax is different
-        trx.insert({
-            hash: hash,
-            email: email
-        })
-        .into('login')
-        .returning('email')
-        .then(loginEmail => {
-            // Be aware of the syntax - different from above!
-            trx('users')
-            .returning('*')
-            .insert({
-                name: name,
-                email: loginEmail[0].email,
-                joined: new Date()
-            })
-            .then(user => {
-                // returning the user object
-                res.json(user[0]);
-            })
-            .then(trx.commit)
-            .catch(trx.rollback);
-        })
-        .catch(err => res.status(400).json('Unable to register'))
-    })
-})
-    /* We are not going to use below OLD method:
-    database.users.push({
-        id: '125',
-        name: name,
-        email: email,
-        // password: password,
-        entries: 0,
-        joined: new Date()
-    })
-    */
-
-// Get user based on id
-app.get('/profile/:id', (req, res) => {
-    const { id } = req.params;
-    db.select('*').from('users').where({'id': id})
-        .then(user => {
-            if (user.length) {
-                res.json(user[0])
-            } else {
-                res.status(400).json('User Not Found')
-            }
-        })
-        .catch(err => res.status(400).json('Error getting user'))
-    /* database.users.forEach(user => {
-         if (user.id === id) {
-             found = true;
-             return res.json(user);
-         } 
-        })
-    */
-})
-
-// Update the number of "entries"
+app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) })
+app.post('/register', register.handleRegister(db, bcrypt)) 
+app.get('/profile/:id', (req, res) => { profile.handleProfile(req, res, db) })
+// Update the number of "entries", below we will use the old method
 app.put('/image', (req, res) => {
     const { id } = req.body;
     db('users').where({
@@ -178,31 +52,28 @@ app.put('/image', (req, res) => {
     })
     .catch(err => res.status(400).json('Unable to get/update entries'))
   })
-    /* Old method
 
-    let found = false
-    database.users.forEach(user => {
-        if (user.id === id) {
-            found = true;
-            user.entries ++;    
-            return res.json(user.entries);
-        } 
-    })
-    if (!found) {
-        res.status(400).json('User not found')
+app.post('/imageUrl', (req, res) => {
+  stub.PostModelOutputs(
+    {
+        model_id: "a403429f2ddf4b49b307e318f00e528b",
+        inputs: [
+            {data: {image: {url: req.body.input}}}
+        ]
+    },
+    metadata,
+    (err, response) => {
+        if (err) {
+            throw new Error(err);
+        }
+        if (response.status.code !== 10000) {
+            throw new Error("Post model outputs failed, status: " + response.status.description);
+        }
+        return res.json(response);
     }
-    */
+  )
+})
 
 app.listen(3001, () => {
     console.log('App in running on port 3001')
 });
-
-/*
-**************Brainstorm session***************
-/                                       --> res = this is working
-/signin                                 --> POST (send user information); then res success/fail
-/register                               --> POST (add data to database); return new user object
-/profile/:userId (with optional param)  --> GET; return user
-/image                                  --> PUT (update user info); return updated user object
-
-*/
